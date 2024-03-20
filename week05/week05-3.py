8a7f79fbe9d976caba6ade273abef9dcd52376c5
yingchi1130 <kiki001130@gmail.com>
2024-03-20 10:50:02 +0800

    week05

---
+++ week05/week05-3.py
@@ -0,0 +1,8 @@
+for i in range(9): #0...8 (小於9),共9個
+  print('i是', i, end=' ')
+print() #跳行
+for i in range(1,9+1): #1...9(左右都+1格)
+  print('i是', i ,end=' ')
+print() #
+for i in range(9-1,-1,-1):
+  print('i是', i, end=' ')
