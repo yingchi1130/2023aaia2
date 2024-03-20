32d9389e15f89b7e72bf362915e5a3add476dff4
yingchi1130 <kiki001130@gmail.com>
2024-03-20 11:58:21 +0800

    week05

---
+++ week05/week05-5.c
@@ -0,0 +1,13 @@
+// LeetCode 2078. Two Furthest Houses With Different Colors
+// 用兩個for迴圈, 左手i,右手j
+int maxDistance(int* colors, int colorsSize) {
+    int ans = 0;
+    for(int i=0; i<colorsSize; i++){
+        for(int j=0; j<colorsSize; j++){
+            if(colors[i] != colors[j] && j-i>ans){
+                ans = j-i;
+            }
+        }
+    }
+    return ans;
+}
