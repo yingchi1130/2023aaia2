e4591b62417ad94a59e7e48e484a23f10d8f442d
yingchi1130 <kiki001130@gmail.com>
2024-04-24 10:59:46 +0800

    week10

---
+++ week10/week10-3.cpp
@@ -0,0 +1,18 @@
+///揭セ材5彻 ㄧΑ/ㄧ计 function
+/// Page 5-5, 5-6
+#include <stdio.h>
+/// Τ把计秈ㄓ int a
+void myPrint(int a){
+    for(int i=0; i<a; i++) printf("琍");
+    printf("\n");
+    ///⊿Τreturn, ⊿Τ把计
+}
+
+int main()
+{ ///璶ㄧΑ main()
+    myPrint(5);
+    myPrint(4);
+    myPrint(3);
+    myPrint(2);
+    myPrint(1);
+}
