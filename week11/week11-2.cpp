0bae0cdc02acea440e2fabb3112dee5455bd63f5
yingchi1130 <kiki001130@gmail.com>
2024-05-01 10:33:57 +0800

    week11

---
+++ week11/week11-2.cpp
@@ -0,0 +1,14 @@
+//week11-2.cpp
+#include <stdio.h>
+int func(int n) {
+    if(n==1) return 1;
+    return n * func(n-1);
+}
+int main()
+{
+    printf("請輸入1個數字: 4");
+    int n=4;
+    //scanf("%d", &n);
+    int ans = func(n);
+    printf("答案是:%d", ans);
+}
